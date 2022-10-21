8d253df10348117fde7050f0c69790c296d7d41c
JoaorGuimaraes <joaoprogramacao123@gmail.com>
2022-10-21 13:59:09 -0300

    github auth

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,28 +1,22 @@
-import { FaGithub } from "react-icons/fa"
-import { FiX } from "react-icons/fi"
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
-
-import styles from "./styles.module.scss"
-
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export function SignInButton() {
+    const { data: session } = useSession();
 
-    const isUserLogged = false;
-
-    return isUserLogged ? (
-        <button
-            type="button"
-            className={styles.signInButton}>
-            <FaGithub color="#04b361" />
-            João Guimarães
-            <FiX color="#737380" className={styles.closeIcon}/>
+    return session ? (
+        <button type="button" onClick={() => signOut()} className={styles.signInButton}>
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button
-            type="button"
-            className={styles.signInButton}>
+        <button type="button" onClick={() => signIn('github')} className={styles.signInButton}>
             <FaGithub color="#eba417" />
-            Sign In WIth Github
+            Sign in with Github
         </button>
-    )
+    );
 }
